c8b249ca765a04aa40ba483a3a3b8ef2b753c1a5
Chan <114750101+Chan4Edu@users.noreply.github.com>
2022-10-26 09:24:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.header ('Breakfast Menu')
 streamlit.text ('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text ('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text ('🥑🍞 Avacado Toast')
 
 
 
